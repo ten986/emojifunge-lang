0817cf0552b610abb411508ca27b25818d8c2c57
ten986 <tenkichi986@yahoo.co.jp>
2021-02-28 09:16:35 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['simple-import-sort'],
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/default': 'off',
+    'import/order': 'off',
+    'no-console': 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'simple-import-sort/imports': 'error',
+    'sort-imports': 'off',
+  },
+  overrides: [
+    {
+      files: '*.js',
+      rules: {
+        'import/order': ['error', { 'newlines-between': 'always' }],
+        'simple-import-sort/imports': 'off',
+      },
+    },
+    {
+      files: ['*.{config,d,test}.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# emojifunge
+
+## これは何
+
+befunge + emoji
+
+ありそうでなかったので作った
+
+## 移動に関する仕様
+
+piet的な感じ？
+ループするか
+
+↑決めたら書く
+
+## memo
+
+` Command + Control + Space ` で出せる
+
+## コマンド一覧
+
+| emoji | 機能 | example |
+|---|---|---|
+|1  |2  |3  |
